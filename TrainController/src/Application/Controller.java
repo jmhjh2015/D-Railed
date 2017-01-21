06644df3d533cd27813c2d28bcf713efdeee8ce2
Aadu Pirn <aap75@pitt.edu>
2017-01-21 15:36:03 -0500

    fixed some of these bullshit default naming schemes in the sample project

---
+++ TrainController/src/Application/Controller.java
@@ -0,0 +1,7 @@
+package Application;
+
+/**
+ * Created by aadu on 1/21/17.
+ */
+public class Controller {
+}
--- TrainController/src/Application/Main.java
+++ TrainController/src/Application/Main.java
@@ -1,5 +1,8 @@
-package sample;
+package Application;
 
+/**
+ * Created by aadu on 1/21/17.
+ */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,9 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 750, 900));
         primaryStage.show();
     }
 
