def901ccaa72505ec940d545ec4ac76a4ec1f9a8
Jonathan Selling <jonathanbselling@gmail.com>
2017-01-26 00:32:04 -0500

    Finished Main UI - working on sub parts

--- UI/TrackController/src/Application/Main.java
+++ UI/TrackController/src/Application/Main.java
@@ -12,9 +12,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -30,7 +30,8 @@ public class Main extends Application {
 	//Class Objects
 	private Stage mainStage, sideStage;
 	private Scene mainScene, murphyScene, userInScene, engScene, toTMScene;
-	private Label testLabel;
+	private Label blockLabel, controlLabel, switchLabel, blockIDLabel, openLabel, lightsLabel, crossLabel, stationLabel, heaterLabel, switchAdjLabel, switchIDLAbel, mainBlockLabel, subBlock1Label,subBlock2Label,connectedLabel;
+	private TextField blockID, openStatus, lightsStatus,crossStatus,stationStatus,heaterStatus, switchAdj1, switchAdj2, switchIDText, mainBlockText, subBlock1Text, subBlock2Text, connectedText, notifications;
     private Button murphyButton, userInputsButton, engInputsButton, toTrackModelButton, murphyBreakTrackButton;
 
 
@@ -38,12 +39,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 	    //Set variables
-        int windowWidth = 950;
-        int windowHeight = 250;
+        double windowWidth = 1250;
+        double windowHeight = 500;
         int sideWidth = 400;
         int sideHeight = 200;
         int inset = 25;
-        int colWidth = 100;
         int rowIndex = 0;
         int colIndex = 0;
         String applicationTitle = "Track Controller";
@@ -59,7 +59,10 @@ public class Main extends Application {
         GridPane userInputs = new GridPane();
         GridPane engInputs = new GridPane();
         GridPane toTrackModel = new GridPane();
-
+        GridPane blockInfo = new GridPane();
+        GridPane blockSwitchIDs = new GridPane();
+        GridPane buttonSelect = new GridPane();
+        GridPane switchInfo = new GridPane();
 
         //Create Scenes
         mainScene = new Scene(main, windowWidth, windowHeight);
@@ -68,123 +71,185 @@ public class Main extends Application {
         engScene =  new Scene(engInputs, sideWidth,sideHeight);
         toTMScene =  new Scene(toTrackModel, sideWidth,sideHeight);
 
-        //Initialize Main Buttons
-        murphyButton = new Button("Murphy Controls");
-        userInputsButton = new Button("CTC Inputs");
-        engInputsButton = new Button("Engineer Inputs");
-        toTrackModelButton = new Button("Inputs To Track Model");
 
-        //Initialize Murphy Buttons
-        murphyBreakTrackButton = new Button("Break Track");
-
-
+        //Initialize Main Pane
         main.setAlignment(Pos.TOP_CENTER);
         main.setPadding(new Insets(inset, inset, inset, inset));
         main.setGridLinesVisible(true);
 
-        //Set up items on main screen
-        testLabel = new Label("Test");
-        testLabel.setMinWidth(colWidth);
+
+        //Set up section titles on main screen
+        blockLabel = new Label("Block Info");
+        controlLabel = new Label("Controls");
+        switchLabel = new Label("Switch Info");
+        blockLabel.setMinWidth(windowWidth/3-5);
+        controlLabel.setMinWidth(windowWidth/3-5);
+        switchLabel.setMinWidth(windowWidth/3-5);
+        blockLabel.setFont(Font.font("Garamond",FontWeight.BOLD,20));
+        controlLabel.setFont(Font.font("Garamond",FontWeight.BOLD,20));
+        switchLabel.setFont(Font.font("Garamond",FontWeight.BOLD,20));
+        main.add(blockLabel, 0, 0);
+        main.add(controlLabel, 1, 0);
+        main.add(switchLabel, 2, 0);
+
+        //Block Info Section
+        blockInfo.setVgap(40);
+        //Block label
+        blockIDLabel = new Label("Block ID: ");
+        blockIDLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(blockIDLabel,0,0);
+
+        //Block ID text
+        blockID = new TextField("ID");
+        blockInfo.add(blockID, 1,0);
+
+        //Open label
+        openLabel = new Label("Open Status: ");
+        openLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(openLabel,0,1);
+
+        //Open status text
+        openStatus = new TextField("Open/Closed");
+        blockInfo.add(openStatus, 1,1);
+
+        //Lights label
+        lightsLabel = new Label("Light Status: ");
+        lightsLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(lightsLabel,0,2);
+
+        //Lights status text
+        lightsStatus = new TextField("Greed/Red");
+        blockInfo.add(lightsStatus, 1,2);
+
+        //Crossroads label
+        crossLabel = new Label("Crossroad Light Status: ");
+        crossLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(crossLabel,0,3);
+
+        //Crossroads status text
+        crossStatus = new TextField("On/Off/(N/A)");
+        blockInfo.add(crossStatus, 1,3);
+
+        //Station label
+        stationLabel = new Label("Station Name: ");
+        stationLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(stationLabel,0,4);
+
+        //Station status text
+        stationStatus = new TextField("Name/(N/A)");
+        blockInfo.add(stationStatus, 1,4);
+
+        //Heater label
+        heaterLabel = new Label("Heater Status: ");
+        heaterLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(heaterLabel,0,5);
+
+        //Heater status text
+        heaterStatus = new TextField("On/Off");
+        blockInfo.add(heaterStatus, 1,5);
+
+        //Switch label
+        switchAdjLabel = new Label("Adjacent Switches: ");
+        switchAdjLabel.setFont(new Font("Garamond",16));
+        blockInfo.add(switchAdjLabel,0,6);
+
+        //Switch GridPane
+        switchAdj1 = new TextField("ID/(N/A)");
+        blockSwitchIDs.add(switchAdj1,0,0);
+        switchAdj2 = new TextField("ID/(N/A)");
+        blockSwitchIDs.add(switchAdj2,1,0);
+        blockInfo.add(blockSwitchIDs, 1,6);
+
+        blockInfo.setMinHeight(windowHeight*2/3);
+
+        main.add(blockInfo,0,1,1,2);
+
+        //Set up ButtonPane
+        murphyButton = new Button("Murphy Controls");
+        userInputsButton = new Button("CTC Inputs");
+        engInputsButton = new Button("Engineer Inputs");
+        toTrackModelButton = new Button("Inputs To Track Model");
         murphyButton.setOnAction(e -> MainButtonClicked(e));
-        murphyButton.setMinWidth(colWidth);
         userInputsButton.setOnAction(e -> MainButtonClicked(e));
-        userInputsButton.setMinWidth(colWidth);
         engInputsButton.setOnAction(e -> MainButtonClicked(e));
-        engInputsButton.setMinWidth(colWidth);
         toTrackModelButton.setOnAction(e -> MainButtonClicked(e));
-        toTrackModelButton.setMinWidth(colWidth);
+        murphyButton.setAlignment(Pos.CENTER_LEFT);
+        userInputsButton.setAlignment(Pos.CENTER_RIGHT);
+        engInputsButton.setAlignment(Pos.CENTER_LEFT);
+        toTrackModelButton.setAlignment(Pos.CENTER_RIGHT);
+        buttonSelect.add(murphyButton, 0, 0);
+        buttonSelect.add(userInputsButton, 1, 0);
+        buttonSelect.add(engInputsButton, 0, 1);
+        buttonSelect.add(toTrackModelButton, 1, 1);
+        buttonSelect.setVgap(10);
+        buttonSelect.setHgap(10);
+        main.add(buttonSelect,1,1);
 
-        //Set up main GridPane
-        main.add(murphyButton, 0, 0);
-        main.add(userInputsButton, 0, 1);
-        main.add(engInputsButton, 1, 0);
-        main.add(toTrackModelButton, 1, 1);
-        main.add(testLabel, 10, 2);
+        //Put info pane in
+        notifications = new TextField("Notifications");
+        notifications.setFont(Font.font("Garamond", 20));
+        notifications.setMinHeight(windowHeight/3);
+        main.add(notifications,1,2);
 
+        //Set up switch info
+        switchInfo.setVgap(45);
+
+        //Switch ID label
+        switchIDLAbel = new Label("Switch ID: ");
+        switchIDLAbel.setFont(new Font("Garamond",16));
+        switchInfo.add(switchIDLAbel,0,0);
+
+        //Switch ID text
+        switchIDText = new TextField("ID/(N/A)");
+        switchInfo.add(switchIDText,1,0);
+
+        //Main block label
+        mainBlockLabel = new Label("Main Block: ");
+        mainBlockLabel.setFont(new Font("Garamond",16));
+        switchInfo.add(mainBlockLabel, 0,1);
+
+        //Main block text
+        mainBlockText = new TextField("Main Block ID");
+        switchInfo.add(mainBlockText, 1,1);
+
+        //Sub block 1 label
+        subBlock1Label = new Label("Sub Block 1: ");
+        subBlock1Label.setFont(new Font("Garamond",16));
+        switchInfo.add(subBlock1Label, 0,2);
+
+        //Sub block 1 text
+        subBlock1Text = new TextField("Sub Block 1 ID");
+        switchInfo.add(subBlock1Text, 1,2);
+
+        //Sub block 2 label
+        subBlock2Label = new Label("Sub Block 2: ");
+        subBlock2Label.setFont(new Font("Garamond",16));
+        switchInfo.add(subBlock2Label, 0,3);
+
+        //Sub block 2 text
+        subBlock2Text = new TextField("Sub Block 2 ID");
+        switchInfo.add(subBlock2Text, 1,3);
+
+        //Connected label
+        connectedLabel = new Label("Sub Block Connected: ");
+        connectedLabel.setFont(new Font("Garamond",16));
+        switchInfo.add(connectedLabel, 0,4);
+
+        //Connected text
+        connectedText = new TextField("Sub Block (1 or 2)");
+        switchInfo.add(connectedText, 1,4);
+
+        main.add(switchInfo,2,1,1,2);
+
+        //Initialize Murphy Pane
+
+
+        //Initialize Murphy Buttons
+        murphyBreakTrackButton = new Button("Break Track");
 
         //Set up items on murphy GridPane
         murphyBreakTrackButton.setOnAction(e -> MurphyButtonClicked(e));
         murphy.add(murphyBreakTrackButton, 0, 0);
-
-/*      //Row 1
-		Label trainIDLabel = new Label("Train: ");
-		trainIDLabel.setTextAlignment(TextAlignment.RIGHT);
-		trainIDLabel.setMinWidth(colWidth);
-		trainIDLabel.setAlignment(Pos.CENTER_RIGHT);
-		main.add(trainIDLabel, colIndex, rowIndex);
-		colIndex++;
-
-		Text trainIDText = new Text();
-		trainIDText.setWrappingWidth(colWidth*2);
-		trainIDText.setText("TEMPTRAINID");
-		trainIDText.setTextAlignment(TextAlignment.LEFT);
-		main.add(trainIDText, colIndex, rowIndex, 2, 1);
-		colIndex = colIndex + 2;
-
-		Label speedLabel = new Label("Speed: ");
-		speedLabel.setMinWidth(colWidth*1.5);
-		speedLabel.setTextAlignment(TextAlignment.RIGHT);
-		speedLabel.setAlignment(Pos.CENTER_RIGHT);
-		main.add(speedLabel, colIndex, rowIndex);
-		colIndex++;
-
-		Text speedText = new Text();
-		speedText.setText("TEMPSPEED");
-		speedText.setWrappingWidth(colWidth*1.5);
-		speedText.setTextAlignment(TextAlignment.LEFT);
-		main.add(speedText, colIndex, rowIndex);
-		colIndex++;
-
-		final Button manualBtn = new Button("MANUAL");
-		HBox hManualBtn = new HBox(0);
-		hManualBtn.setMinWidth(colWidth*3);
-		manualBtn.setMinWidth(colWidth*2);
-		manualBtn.setAlignment(Pos.CENTER);
-		hManualBtn.setAlignment(Pos.CENTER_RIGHT);
-		hManualBtn.getChildren().add(manualBtn);
-		main.add(hManualBtn, colIndex, rowIndex, 3, 1);
-		rowIndex++;
-		colIndex = 0;
-
-		//Row 2
-		Label routeLabel = new Label("Route: ");
-		routeLabel.setTextAlignment(TextAlignment.RIGHT);
-		routeLabel.setMinWidth(colWidth);
-		routeLabel.setAlignment(Pos.CENTER_RIGHT);
-		main.add(routeLabel, colIndex, rowIndex);
-		colIndex++;
-
-		Text routeText = new Text();
-		routeText.setWrappingWidth(colWidth*2);
-		routeText.setText("TEMPROUTE");
-		routeText.setTextAlignment(TextAlignment.LEFT);
-		main.add(routeText, colIndex, rowIndex, 2, 1);
-		colIndex = colIndex + 2;
-
-		Label powerLabel = new Label("Route: ");
-		powerLabel.setMinWidth(colWidth*1.5);
-		powerLabel.setTextAlignment(TextAlignment.RIGHT);
-		powerLabel.setAlignment(Pos.CENTER_RIGHT);
-		main.add(powerLabel, colIndex, rowIndex);
-		colIndex++;
-
-		Text powerText = new Text();
-		powerText.setText("TEMPROUTE");
-		powerText.setWrappingWidth(colWidth*1.5);
-		powerText.setTextAlignment(TextAlignment.LEFT);
-		main.add(powerText, colIndex, rowIndex);
-		colIndex++;
-
-		final Button automaticBtn = new Button("AUTOMATIC");
-		HBox hAutomaticBtn = new HBox(0);
-		hAutomaticBtn.setMinWidth(colWidth*3);
-		automaticBtn.setMinWidth(colWidth*2);
-		automaticBtn.setAlignment(Pos.CENTER);
-		hAutomaticBtn.setAlignment(Pos.CENTER_RIGHT);
-		hAutomaticBtn.getChildren().add(automaticBtn);
-		main.add(hAutomaticBtn, colIndex, rowIndex, 3, 1);
-		rowIndex++;
-		colIndex = 0;*/
 
 
         mainStage.setScene(mainScene);
@@ -196,7 +261,7 @@ public class Main extends Application {
         Object source = e.getSource();
         if(source == murphyBreakTrackButton)
         {
-            testLabel.setText("From Murphy");
+
         }
     }
 
