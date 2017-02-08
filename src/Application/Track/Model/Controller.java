83a840c5b771c329088cc14bb5c153a3ef31296a
karatemanz <adz13@pitt.edu>
2017-02-08 00:15:53 -0500

    remove overlapping files

--- src/Application/Track/Model/Controller.java
+++
@@ -1,7 +0,0 @@
-package Application.Track.Model;
-
-/**
- * Created by aadu on 1/21/17.
- */
-public class Controller {
-}
--- src/Application/Train/Controller/Controller.java
+++
@@ -1,7 +0,0 @@
-package Application.Train.Controller;
-
-/**
- * Created by aadu on 1/21/17.
- */
-public class Controller {
-}
